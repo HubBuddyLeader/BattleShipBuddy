bbc7764667de78b02ecc907bfc07a868669a9c7c
Naveen <naveen147@gmail.com>
2016-09-21 12:56:04 +1000

    High score save fixed

--- src/HighScoreController.cs
+++ src/HighScoreController.cs
@@ -198,7 +198,7 @@ static class HighScoreController
 			_Scores.RemoveAt(_Scores.Count - 1);
 			_Scores.Add(s);
 			_Scores.Sort();
-
+			SaveScores();
             GameController.EndCurrentState();
 		}
 	}
